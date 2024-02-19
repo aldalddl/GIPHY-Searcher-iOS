5fe2d84830a83a02c4550a1b57bf27e1c2343102
aldalddl <aldalddl2007@gmail.com>
2024-02-19 21:18:24 +0900

    chore: 프로젝트 생성 및 code base UI 설정

---
+++ GIPHYSearcher/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GIPHYSearcher
+//
+//  Created by 강민지 on 2/19/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+
+}
+

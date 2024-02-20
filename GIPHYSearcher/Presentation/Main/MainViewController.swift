246490cd909088b441bcf604e467e6c55f2d9c00
aldalddl <aldalddl2007@gmail.com>
2024-02-21 03:16:58 +0900

    feat: NavigationBar 및 SearchBar 추가

--- GIPHYSearcher/Presentation/Main/MainViewController.swift
+++ GIPHYSearcher/Presentation/Main/MainViewController.swift
@@ -11,6 +11,19 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()        
+        
+        setUp()
+    }
+    
+    func setUp() {
+        self.view.backgroundColor = .backgroundColor
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "Search GIFs"
+        self.navigationItem.searchController = searchController
+        
+        self.navigationItem.title = "GIPHY Searcher"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 

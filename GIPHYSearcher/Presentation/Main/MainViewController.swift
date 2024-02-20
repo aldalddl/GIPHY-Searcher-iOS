804ba79955da21d315b6ea0696241063ed6c200d
aldalddl <aldalddl2007@gmail.com>
2024-02-21 03:24:44 +0900

    feat: GIF가 보여질 CollectionView 생성

--- GIPHYSearcher/Presentation/Main/MainViewController.swift
+++ GIPHYSearcher/Presentation/Main/MainViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
-
+    let trendingCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumInteritemSpacing = 8
+        flowLayout.minimumLineSpacing = 8
+        flowLayout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 48) / 2, height: 150)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .backgroundColor
+        collectionView.isScrollEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()        
         
         setUp()
+        layout()
     }
     
     func setUp() {
@@ -24,8 +41,37 @@ class MainViewController: UIViewController {
         
         self.navigationItem.title = "GIPHY Searcher"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        trendingCollectionView.dataSource = self
+        trendingCollectionView.delegate = self
+        trendingCollectionView.register(TrendingCollectionViewCell.self, forCellWithReuseIdentifier: "TrendingCollectionViewCell")
     }
-
+    
+    func layout() {
+        self.view.addSubview(trendingCollectionView)
+        
+        trendingCollectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(20)
+        }
+    }
 
 }
 
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO: 샘플 데이터 수를 서버 데이터 수로 대체하는 작업 필요
+        return 13
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrendingCollectionViewCell", for: indexPath) as? TrendingCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        // TODO: 샘플 데이터를 서버 데이터로 대체하는 작업 필요
+        cell.testImageView.image = UIImage(named: "TestImage")
+        
+        return cell
+    }
+    
+}

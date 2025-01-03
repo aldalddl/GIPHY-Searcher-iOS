ec85aa949c7ee822c906e76e0f7014de3a400d0a
aldalddl <aldalddl2007@gmail.com>
2025-01-03 19:48:09 +0900

    test: Mock URLSession 생성

---
+++ GIPHYSearcherTests/MockURLSession.swift
@@ -0,0 +1,33 @@
+//
+//  MockURLSession.swift
+//  GIPHYSearcher
+//
+//  Created by 강민지 on 12/13/24.
+//
+
+import Foundation
+import GIPHYSearcher
+
+protocol URLSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol { }
+
+class MockURLSession: URLSessionProtocol {
+    typealias Response = (data: Data?, reponse: URLResponse?, error: Error?)
+    
+    let response: Response
+    
+    init(response: Response) {
+        self.response = response
+    }
+    
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return MockURLSessionDataTask(resumeHandler: {
+            completionHandler(self.response.data,
+                              self.response.reponse,
+                              self.response.error)
+        })
+    }
+}

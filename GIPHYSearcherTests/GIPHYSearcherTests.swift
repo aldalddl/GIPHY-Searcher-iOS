f0377398fc38c46c95bf85b381037f27c24e0cf5
aldalddl <aldalddl2007@gmail.com>
2025-01-03 20:09:22 +0900

    test: XCTest를 사용한 API 호출 Unit Test 구현

--- GIPHYSearcherTests/GIPHYSearcherTests.swift
+++ GIPHYSearcherTests/GIPHYSearcherTests.swift
@@ -6,30 +6,47 @@
 //
 
 import XCTest
+@testable import GIPHYSearcher
 
 final class GIPHYSearcherTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testFetchTrending() {
+        let url = "\(API.baseURL)\(API.Endpoint.trending)"
+        guard let validUrl = URL(string: url) else {
+            XCTFail("URL 생성 실패")
+            return
         }
+        
+        let mockResponse: MockURLSession.Response = {
+            let data = JsonLoader.data(fileName: "MockTrending")
+            let successResponse = HTTPURLResponse(url: validUrl,
+                                                  statusCode: 200,
+                                                  httpVersion: nil,
+                                                  headerFields: nil)
+            return (data: data, reponse: successResponse, error: nil)
+        }()
+        
+        let mockURLSession = MockURLSession(response: mockResponse)
+        let mockDelegate = MockDelegate()
+        let sut = GiphyAPIManager(delegate: mockDelegate, session: mockURLSession)
+        
+        sut.fetchTrending()
+        
+        XCTAssertEqual(mockDelegate.receivedData?.count, 1, "데이터 개수가 일치하지 않음")
+        XCTAssertEqual(mockDelegate.receivedData?.first?.title, "TestGIF",  "GIF 이름이 예상과 다름")
+        XCTAssertEqual(mockDelegate.receivedData?.first?.url, "https://example.com/gif1")
+        XCTAssertNil(mockDelegate.receivedError)
     }
+}
 
+final class MockDelegate: GiphyAPIManagerDelegate {
+    var receivedData: [gifDataModel]?
+    var receivedError: NetworkError?
+    
+    func didUpdateData(data: [gifDataModel]) {
+        receivedData = data
+    }
+    
+    func didFailWithError(error: NetworkError) {
+        receivedError = error
+    }
 }
